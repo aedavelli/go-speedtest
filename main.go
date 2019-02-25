038f3a9e5ec38b46eeeb3e4417af5bf907dda643
Vijaya Simha Reddy Aedavelli <9965118+aedavelli@users.noreply.github.com>
2019-02-24 17:32:42 -0800

    Added travis-ci.yml

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
 	_ "github.com/aedavelli/go-speedtest/speedtest"
 )
 
+var listenPort int
+var listenIP string
+
+func init() {
+	flag.IntVar(&listenPort, "p", 8080, "SpeedTest listen port")
+	flag.StringVar(&listenIP, "i", "", "SpeedTest listen IP")
+}
+
 func main() {
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	flag.Parse()
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", listenIP, listenPort), nil))
 }
